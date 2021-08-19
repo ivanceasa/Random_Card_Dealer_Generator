f9787633eb253c25d12b9720355d2372b99a3b66
ivanceasa <ceaivan777@gmail.com>
2021-08-19 14:10:52 +0000

    Random card dealer generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  card.classList.add(generateRandomSuit());
+  card.innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = () => {
+  let suits = ["diamond", "heart", "club", "spade"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
